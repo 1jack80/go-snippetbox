4513c60c4d9ef83ac9f35bf2a991a293370a1f3b
1jack80 <work.davidmainoo@gmail.com>
2023-06-03 02:37:06 +0000

    inject loggers as dependencies for the route handlers to utilize

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request){ 
+func (app *application) home(w http.ResponseWriter, r *http.Request){ 
   if r.URL.Path != "/" {
     http.NotFound(w, r)
     return
@@ -22,14 +21,14 @@ func home(w http.ResponseWriter, r *http.Request){
 
   ts, err := template.ParseFiles(files...);
   if err != nil {
-    log.Println(err.Error())
+    app.errorLog.Println(err.Error())
     http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     return
   }
 
   err = ts.ExecuteTemplate(w, "base", nil)
   if err != nil {
-    log.Println(err.Error())
+    app.errorLog.Println(err.Error())
     http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     return
   }
@@ -37,7 +36,7 @@ func home(w http.ResponseWriter, r *http.Request){
 }
 
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func  (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
   id, err := strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
     http.Error(w, "invalid id", http.StatusNotFound)
@@ -47,7 +46,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Get specific snippet with id %d", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request){ 
+func  (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){ 
   if r.Method != http.MethodPost {
     w.Header().Set("Allow", http.MethodPost)
     http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+  errorLog *log.Logger
+  infoLog *log.Logger
+}
+
 func main() {
   addr := flag.String("addr", "4000", "Specifies the port for the server to startup on.");
   flag.Parse();
@@ -14,13 +19,18 @@ func main() {
   infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
   errLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+  app := application {
+    errorLog: errLog,
+    infoLog: infoLog,
+  }
+
   mux := http.NewServeMux()
   fileserver := http.FileServer(http.Dir("./ui/static/"))
   mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/snippet/view", snippetView)
-  mux.HandleFunc("/snippet/create", snippetCreate)
+  mux.HandleFunc("/", app.home)
+  mux.HandleFunc("/snippet/view",app.snippetView)
+  mux.HandleFunc("/snippet/create", app.snippetCreate)
 
   infoLog.Printf("Server started on port %s\n", *addr);
 
