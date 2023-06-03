daadaa2e8cf3598233d7c0459b226e62e4b4247c
1jack80 <work.davidmainoo@gmail.com>
2023-06-03 03:01:51 +0000

    seperate error logs and responses from main code == prevent repeated code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,51 +7,48 @@ import (
 	"strconv"
 )
 
-func (app *application) home(w http.ResponseWriter, r *http.Request){ 
-  if r.URL.Path != "/" {
-    http.NotFound(w, r)
-    return
-  }
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
 
-  files := []string{
-    "./ui/html/base.html",
-    "./ui/html/partials/nav.html",
-    "./ui/html/pages/home.html",
-  }
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
+	}
 
-  ts, err := template.ParseFiles(files...);
-  if err != nil {
-    app.errorLog.Println(err.Error())
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-    return
-  }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-  err = ts.ExecuteTemplate(w, "base", nil)
-  if err != nil {
-    app.errorLog.Println(err.Error())
-    http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-    return
-  }
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
 }
 
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.clientError(w, http.StatusNotFound)
+		return
+	}
 
-func  (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-  id, err := strconv.Atoi(r.URL.Query().Get("id"))
-  if err != nil || id < 1 {
-    http.Error(w, "invalid id", http.StatusNotFound)
-    return
-  }
-
-  fmt.Fprintf(w, "Get specific snippet with id %d", id)
+	fmt.Fprintf(w, "Get specific snippet with id %d", id)
 }
 
-func  (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){ 
-  if r.Method != http.MethodPost {
-    w.Header().Set("Allow", http.MethodPost)
-    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    return
-  }
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
 
-  fmt.Fprintf(w, "Create a new snippet")
+	fmt.Fprintf(w, "Create a new snippet")
 }
