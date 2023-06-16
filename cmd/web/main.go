4d34e2ffc3914772e2652aa91a47d495038c339f
1jack80 <work.davidmainoo@gmail.com>
2023-06-16 01:00:08 +0000

    add database drivers to application and initialize connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,10 +17,19 @@ type application struct {
 
 func main() {
   addr := flag.String("addr", "4000", "Specifies the port for the server to startup on.");
+  dsn := flag.String("dsn", "web:mdk00@web@/snippetbox?parseTime=true", "MySql data source name")
+
   flag.Parse();
+  
 
   infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
   errLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+  db, err := openDB(*dsn)
+  if err != nil {
+    errLog.Fatal(err)  
+  }
+  defer db.Close()
 
   app := application {
     errorLog: errLog,
@@ -30,7 +42,18 @@ func main() {
     Handler: app.routes(),
   }
   infoLog.Printf("Server started on port %s\n", *addr);
-  err := server.ListenAndServe()
+  err = server.ListenAndServe()
 
   errLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+  db,err := sql.Open("mysql", dsn)
+  if err != nil {
+    return nil, err
+  }
+  if err = db.Ping(); err != nil {
+    return nil, err
+  }
+  return db, nil
 }
